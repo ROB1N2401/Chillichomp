cfa1f1e44da33d3c580f10e1aad1ffd11543bacb
ROB1N2401 <60138872+ROB1N2401@users.noreply.github.com>
2020-04-23 16:59:28 +0200

    Playtest Prototype Version

---
+++ Chillichomp/Chillichomp/Assets/Prototype/Scripts/Countdown.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+
+public class Countdown : MonoBehaviour
+{
+    public float startingTime_ = 5.5f;
+    public TextMeshProUGUI text_;
+    public GameObject victory_;
+
+    [SerializeField] private UnityEvent disableControls_;
+    private bool instantiate_victory_condition_ = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        text_.text = startingTime_.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (startingTime_ > 0)
+        {
+            startingTime_ -= Time.deltaTime;
+            //Debug.Log(startingTime_);
+        }
+
+        text_.text = Mathf.Round(startingTime_).ToString();
+
+        if (startingTime_ <= 0)
+        {
+            if (instantiate_victory_condition_ == true)
+            { 
+                GameObject f = Instantiate(victory_) as GameObject;
+                f.transform.position = Camera.main.transform.position;
+                instantiate_victory_condition_ = false;
+            }
+
+            disableControls_.Invoke();
+
+            StartCoroutine(RestartGame());
+        }
+    }
+
+    IEnumerator RestartGame()
+    {
+        //yield return new WaitForSeconds(0.1f);
+        yield return new WaitForSeconds(4f);
+        SceneManager.LoadScene("PrototypeScene");
+    }
+}
---
+++ Chillichomp/Chillichomp/Assets/Prototype/Scripts/Thermometer.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Thermometer : MonoBehaviour
+{
+    internal int currentLevel_; //spiciness level
+
+    [SerializeField] private List<Sprite> allSprites_; //storing sprites of different states, where 0 is the lowest temperature and 3 is overheating
+    private SpriteRenderer sr_;
+
+    void Awake()
+    {
+        sr_ = GetComponent<SpriteRenderer>();
+    }
+
+    void Start()
+    {
+        currentLevel_ = 0;
+    }
+
+    public void IncreaseTemperature(int spiciness_in)
+    {
+        currentLevel_ += spiciness_in;
+    }
+
+    public void DecreaseTemperature(int spiciness_in)
+    {
+        currentLevel_ -= spiciness_in;
+    }
+
+    IEnumerator CoolDown()
+    {
+        FoodInteraction a = FindObjectOfType<FoodInteraction>();
+        DeployFood b = FindObjectOfType<DeployFood>();
+        a.Overheat();
+
+        yield return new WaitForSeconds(0.1f);
+
+        a.enabled = false;
+        b.enabled = false;
+
+        yield return new WaitForSeconds(3f);
+
+        a.enabled = true;
+        b.enabled = true;
+
+        currentLevel_ = 0;
+        a.CloseMouth();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(currentLevel_ >= 0 && currentLevel_ <= 7)
+        {
+            sr_.sprite = allSprites_[currentLevel_];
+        }
+        else if (currentLevel_ > 7)
+        {
+            sr_.sprite = allSprites_[8];
+            StartCoroutine(CoolDown());
+        }
+    }
+}
