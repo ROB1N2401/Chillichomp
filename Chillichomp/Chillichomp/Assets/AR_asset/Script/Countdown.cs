2f4c0e95cc010c2fa2a5bd59e5d098946446380b
ROB1N2401 <60138872+ROB1N2401@users.noreply.github.com>
2020-05-13 13:57:03 +0200

    Timer implementation
    -Added 'Countdown.cs' script
    -Added 'TextRevealEffect.cs' script
    -Added victory prefab
    -Implemented new scripts into the game

---
+++ Chillichomp/Chillichomp/Assets/AR_asset/Script/Countdown.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+
+public class Countdown : MonoBehaviour
+{
+    public float StartingTime;
+    public string SceneName;
+    //public TextMeshProUGUI Text;
+    public GameObject Victory;
+
+    [SerializeField] private UnityEvent _disableControls;
+    private bool _instantiateVictoryCondition = true;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (StartingTime > 0)
+        {
+            StartingTime -= Time.deltaTime;
+            //Debug.Log(startingTime_);
+        }
+
+        //Text.text = Mathf.Round(StartingTime).ToString();
+
+        if (StartingTime <= 0)
+        {
+            if (_instantiateVictoryCondition == true)
+            { 
+                GameObject f = Instantiate(Victory) as GameObject;
+                f.transform.position = Camera.main.transform.position;
+                _instantiateVictoryCondition = false;
+            }
+
+            _disableControls.Invoke();
+
+            StartCoroutine(RestartGame());
+        }
+    }
+
+    IEnumerator RestartGame()
+    {
+        //yield return new WaitForSeconds(0.1f);
+        yield return new WaitForSeconds(4f);
+        SceneManager.LoadScene(SceneName);
+    }
+}
---
+++ Chillichomp/Chillichomp/Assets/AR_asset/Script/TextRevealEffect.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TextRevealEffect : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI _textMeshProUGUI = null;
+    private char[] _letters = { };
+
+    public float RevealTime;
+    [TextArea] public string Text;
+ 
+    // Start is called before the first frame update
+    void Start()
+    {
+        _letters = Text.ToCharArray();
+        StartCoroutine(SpawnText());
+    }
+
+    IEnumerator SpawnText()
+    {
+        //_textMeshProUGUI = GetComponent<TextMeshProUGUI>();
+        // ?? gameObject.AddComponent<TextMeshPro>();
+        for (int i = 0; i < _letters.Length; i++)
+        {
+            _textMeshProUGUI.text += _letters[i];
+            yield return new WaitForSeconds(RevealTime);
+        }
+    }
+}
