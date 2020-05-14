7fc60cbe676e6458e9043aa9a5d71bd7d52bd7db
ROB1N2401 <60138872+ROB1N2401@users.noreply.github.com>
2020-05-14 16:10:42 +0200

    Branch first commit
    -Created 'TutorialControl.cs'
    -Created first version of the tutorial window

---
+++ Chillichomp/Chillichomp/Assets/AR_asset/Script/TutorialControl.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class TutorialControl : MonoBehaviour
+{
+    [SerializeField] [TextArea] private List<string> _text = new List<string>(); //list designated for storing all tutorial explanation texts
+    [SerializeField] private TextMeshProUGUI _tutorialTextTMP = null;
+    [SerializeField] private TextMeshProUGUI _buttonTextTMP = null;
+    private int _currentText = 0;
+
+    private void Awake()
+    { 
+        if (_text != null)
+        {
+            _tutorialTextTMP.text = _text[_currentText];
+        }
+        else
+            Debug.LogError("TutorialConrol.cs: text list is empty");
+    }
+
+    public void CallNext()
+    {
+        _currentText += 1;
+
+        if (_currentText == _text.Count - 1)
+        {
+            _buttonTextTMP.text = "Close";
+        }
+
+        if (_currentText <= _text.Count - 1)
+        {
+            _tutorialTextTMP.text = _text[_currentText];
+        }
+        else
+        {
+            gameObject.SetActive(false);
+            //Destroy(gameObject);
+        }
+    }
+}
