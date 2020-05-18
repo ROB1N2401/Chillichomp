13b768638ed26d904810c5027da488318c1783dc
ROB1N2401 <60138872+ROB1N2401@users.noreply.github.com>
2020-05-18 17:00:51 +0200

    Updated beta tutorial
    -Changed fonts everywhere
    -Added contextualized images for the tutorial
    -Made some changes within the Menu Scene

--- Chillichomp/Chillichomp/Assets/AR_asset/Script/TutorialControl.cs
+++ Chillichomp/Chillichomp/Assets/AR_asset/Script/TutorialControl.cs
@@ -7,15 +7,19 @@ using TMPro;
 public class TutorialControl : MonoBehaviour
 {
     [SerializeField] [TextArea (14, 10)] private List<string> _text = new List<string>(); //list designated for storing all tutorial explanation texts
+    [SerializeField] private GameObject _imageShowcase = null;
     [SerializeField] private TextMeshProUGUI _tutorialTextTMP = null;
     [SerializeField] private TextMeshProUGUI _buttonTextTMP = null;
     [SerializeField] private Image _imageComponent = null;
     [SerializeField] private List<Sprite> _sprites = new List<Sprite>();
     private int _currentText = 0;
+    private bool _showImage = false;
 
     private void Awake()
     {
         _buttonTextTMP.text = "Next";
+
+        _imageShowcase.SetActive(false);
 
         if (_text != null)
         {
@@ -25,13 +29,30 @@ public class TutorialControl : MonoBehaviour
             Debug.LogError("TutorialConrol.cs: text list is empty");
     }
 
+    private void Update()
+    {
+        _imageShowcase.SetActive(_showImage);
+    }
+
     public void CallNext()
     {
+        _showImage = false;
         _currentText += 1;
 
         if (_currentText == _text.Count - 1)
         {
             _buttonTextTMP.text = "Close";
+        }
+
+        if (_currentText == 3)
+        {
+            _showImage = true;
+            _imageComponent.sprite = _sprites[0];
+        }
+        else if (_currentText == 4)
+        {
+            _showImage = true;
+            _imageComponent.sprite = _sprites[1];
         }
 
         if (_currentText <= _text.Count - 1)
